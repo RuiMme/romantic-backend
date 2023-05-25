fb133124c852225cc407023f2b812393e7b2a944
李绍坚 <lishaojian32@gmail.com>
2023-05-25 15:51:00 +0800

    Initial commit

---
+++ src/main/java/com/romantic/romanticbackend/RomanticBackendApplication.java
@@ -0,0 +1,13 @@
+package com.romantic.romanticbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanticBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RomanticBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/romantic/romanticbackend/RomanticBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romantic.romanticbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomanticBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
